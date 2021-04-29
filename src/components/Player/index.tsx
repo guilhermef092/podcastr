78a056cd3acfdcfb0714b36ef8a3c398fb6579fd
Guilherme Figueiredo <guilherme.figueiredo092@gmail.com>
2021-04-29 16:48:04 -0300

    Player: Next and previous control

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -10,7 +10,7 @@ import styles from './styles.module.scss';
 export function Player() {
   const audioRef = useRef<HTMLAudioElement>(null);
 
-  const { episodeList, currentEpisodeIndex, isPlaying, togglePlay, setPlayingState } = useContext(PlayerContext);
+  const { episodeList, currentEpisodeIndex, isPlaying, togglePlay, setPlayingState, playNext, playPrevious } = useContext(PlayerContext);
 
   useEffect(() => {
     if (!audioRef.current) {
@@ -79,7 +79,7 @@ export function Player() {
             <img src="/shuffle.svg" alt="Embaralhar" />
           </button>
 
-          <button type="button" disabled={!episode}>
+          <button type="button" onClick={playPrevious} disabled={!episode}>
             <img src="/play-previous.svg" alt="Tocar anterior" />
           </button>
 
@@ -89,7 +89,7 @@ export function Player() {
               : <img src="/play.svg" alt="Tocar" />}
           </button>
 
-          <button type="button" disabled={!episode}>
+          <button type="button" onClick={playNext} disabled={!episode}>
             <img src="/play-next.svg" alt="Tocar próxima" />
           </button>
 
